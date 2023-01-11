bec0f134bf1c64a5f5e7dd6dc28b9a4dfaf8a06c
RegisSeki <regisseki@gmail.com>
2023-01-11 09:42:42 -0300

    Add existing files to GIT - First commit

---
+++ src/test/java/com/rtseki/witch/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtseki.witch.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
