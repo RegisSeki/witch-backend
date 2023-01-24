426789b5320730fd2dc66e506848a96c0ef90603
RegisSeki <regisseki@gmail.com>
2023-01-24 17:29:17 -0300

    Add repository annotation

--- src/main/java/com/rtseki/witch/backend/domain/repository/CategoryRepository.java
+++ src/main/java/com/rtseki/witch/backend/domain/repository/CategoryRepository.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rtseki.witch.backend.domain.model.Category;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 	Page<Category> findAll(Pageable pageable);
