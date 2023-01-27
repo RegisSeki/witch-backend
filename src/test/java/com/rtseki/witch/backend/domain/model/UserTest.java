efd80cadcb13d4b641facedffb4392399a369015
RegisSeki <regisseki@gmail.com>
2023-01-27 07:02:05 -0300

    Remove unnecessary import

--- src/test/java/com/rtseki/witch/backend/domain/model/UserTest.java
+++ src/test/java/com/rtseki/witch/backend/domain/model/UserTest.java
@@ -1,13 +1,11 @@
 package com.rtseki.witch.backend.domain.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.UUID;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
