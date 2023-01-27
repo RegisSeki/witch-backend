f7b31687c078b50286503baf03b667fbf2af2cd0
RegisSeki <regisseki@gmail.com>
2023-01-27 07:25:27 -0300

    Remove import at CategoryTest

--- src/test/java/com/rtseki/witch/backend/domain/model/CategoryTest.java
+++ src/test/java/com/rtseki/witch/backend/domain/model/CategoryTest.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import com.rtseki.witch.backend.domain.exception.ResourceNotFoundException;
 import com.rtseki.witch.backend.domain.repository.CategoryRepository;
 
 import jakarta.persistence.PersistenceException;
