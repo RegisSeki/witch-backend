0e456aa6f1ec3660068a7fc0ea6a36afb1cf021d
RegisSeki <regisseki@gmail.com>
2023-02-06 14:57:54 -0300

    Add jwt invalid token handler exception

--- src/main/java/com/rtseki/witch/backend/domain/config/JwtAuthenticationFilter.java
+++ src/main/java/com/rtseki/witch/backend/domain/config/JwtAuthenticationFilter.java
@@ -32,35 +32,47 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		@NonNull HttpServletResponse response, 
 		@NonNull FilterChain filterChain
 	)throws ServletException, IOException {
-		final String authHeader = request.getHeader("Authorization");
-		final String jwtToken;
-		final String userEmail;
-		
-		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-			filterChain.doFilter(request, response);
+		try {
+			final String authHeader = request.getHeader("Authorization");
+			final String jwtToken;
+			final String userEmail;
+			
+			if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+				filterChain.doFilter(request, response);
+				return;
+			}
+			
+			jwtToken = authHeader.substring(7);
+			userEmail = jwtService.extractUsername(jwtToken);
+						
+			if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+				UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+				
+				if (jwtService.isTokenValid(jwtToken, userDetails)) {
+					UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+						userEmail,
+						null,
+						userDetails.getAuthorities()
+					);
+					
+					authToken.setDetails(
+						new WebAuthenticationDetailsSource().buildDetails(request)
+					);
+					
+					SecurityContextHolder.getContext().setAuthentication(authToken);
+				}
+			}
+		}catch(Exception e) {
+			handleInvalidJwt(response);
 			return;
 		}
 		
-		jwtToken = authHeader.substring(7);
-		userEmail = jwtService.extractUsername(jwtToken);
-		
-		if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-			UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-			
-			if (jwtService.isTokenValid(jwtToken, userDetails)) {
-				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-					userEmail,
-					null,
-					userDetails.getAuthorities()
-				);
-				
-				authToken.setDetails(
-					new WebAuthenticationDetailsSource().buildDetails(request)
-				);
-				
-				SecurityContextHolder.getContext().setAuthentication(authToken);
-			}
-		}
 		filterChain.doFilter(request, response);
-	}	
+	}
+	
+	  private void handleInvalidJwt(HttpServletResponse response) throws IOException {
+	    response.setContentType("application/json");
+	    response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+	    response.getWriter().write("Token not valid");
+	  }
 }
